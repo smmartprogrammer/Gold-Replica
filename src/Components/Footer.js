04696720234690a29c99dea6255bde6c5b66657f
Raghunathji <partabrajp@gmail.com>
2021-08-26 07:30:55 +0500

    updated the UI and functinality

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -31,40 +31,40 @@ const Footer = () => {
           <img src="https://cdn.shopify.com/s/files/1/0362/7771/4988/files/ezgif.com-gif-maker.png?v=1615563773" />
         </div>
       </div>
-      <div className="rateYearAndTraditions">
-        <div className="goldRate">
-          <h3>Gold rate today (old gold):</h3>
-          <br />
-          <p>Information on buying gold</p>
-          <p>data protection</p>
-          <p>imprint</p>
-          <p>040 - 600 5533 00</p>
-        </div>
-        <div className="invitation">
-          <h3>Invitation to events once a year</h3>
-          <div className="registrationField">
-            {/* <form class="example" action="action_page.php">
-            <input type="text" placeholder="Search.." name="search">
-            <button type="submit"><i class="fa fa-search"></i></button>
-            </form> */}
-            {/* <input
-              type="email"
-              className="registration"
-              placeholder="E-mail Address"
-            />
-            <button className="registrationButton">REGISTRATION</button> */}
-          </div>
-        </div>
-        <div className="traditionyears">
-          <h3>90 years of tradition</h3>
-          <br />
-          <p>
-            Since 2018 carbon neutral and RE-
+      <div className="addressPlace">
+        <div className="rateYearAndTraditions">
+          <div className="goldRate">
+            <h3>Gold rate today (old gold):</h3>
             <br />
-            GOLD
-          </p>
-          <p>since 2019 plastic neutral</p>
-          <p>since 2020 FAIR MINED certified</p>
+            <p>Information on buying gold</p>
+            <p>data protection</p>
+            <p>imprint</p>
+            <p>040 - 600 5533 00</p>
+          </div>
+          <div className="invitation">
+            <h3>Invitation to events once a year</h3>
+            <div className="registrationField">
+              <div className="fieldsSections">
+                <div className="inputField">
+                  <input type="email" className="registration" />
+                </div>
+                <div className="inputSecondField">
+                  <button className="registrationButton">Registration</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="traditionyears">
+            <h3>90 years of tradition</h3>
+            <br />
+            <p>
+              Since 2018 carbon neutral and RE-
+              <br />
+              GOLD
+            </p>
+            <p>since 2019 plastic neutral</p>
+            <p>since 2020 FAIR MINED certified</p>
+          </div>
         </div>
       </div>
     </div>
--- src/Components/PriceComponent.js
+++ src/Components/PriceComponent.js
@@ -1,7 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../Stylesheet/PriceComponent.css';
 
 const PriceComponent = () => {
+  const [price, setPrice] = useState(0);
+  const [gram, setGram] = useState(0);
+  const [nineHunderdCoins, setNineHundredCoins] = useState(900);
+  const [sevenFiftyGold, setSevenFiftyGold] = useState(750);
+  const [fiveEightyFiveGold, setFiveEightyFiveGold] = useState(585);
+  const [threeHundredThirtyThreeGold, setThreeHundredThirtyThreeGold] =
+    useState(333);
+  const [currentOption, setCurrentOption] = useState();
+  const [inputedGoldPrice, setInputedGoldPrice] = useState(0);
+  const [active, setActive] = useState(0);
+  const [newGram, setNewGram] = useState();
+  const [newValue, setNewValue] = useState(0);
+
+  const priceHandling = (e) => {
+    setPrice(e.target.value);
+  };
+
+  const gramWeightHandling = (e) => {
+    setGram(e.target.value);
+    console.log(gram);
+  };
+
+  const handleSubmit = () => {
+    setInputedGoldPrice(price);
+    setCurrentOption(gram);
+    setActive(currentOption);
+  };
+
+  const firstbtn = () => {
+    setCurrentOption(nineHunderdCoins);
+  };
+  const secondBtn = () => {
+    setCurrentOption(sevenFiftyGold);
+  };
+
+  const thirdBtn = () => {
+    setCurrentOption(fiveEightyFiveGold);
+  };
+
+  const fourthBtn = () => {
+    setCurrentOption(threeHundredThirtyThreeGold);
+  };
+
+  useEffect(async () => {
+    const response = await fetch(
+      'https://metals-api.com/api/latest?access_key=88mblef0aaiiuj5zmafdtinn9yvy76cgb49ri64116dx08pnf0xibw6axp5b',
+    );
+    console.log('response =', response);
+
+    const data = await response.json();
+    console.log('Data', data);
+  });
+
   return (
     <div className="priceMain">
       <div className="priceMain_first">
@@ -12,32 +65,47 @@ const PriceComponent = () => {
               className="number"
               type="number"
               placeholder="Please enter the Price"
-              required
+              // value={value}
+              onChange={priceHandling}
             />
+
             <input
               className="number"
               type="number"
               placeholder="Please enter the weights in grams"
-              required
+              onChange={gramWeightHandling}
+              // onChange={(e) => (e.target.value)}
             />
             <br />
-            <button className="btn">900 Coin</button>
-            <button className="btn">750 Gold</button>
-            <button className="btn">585 Gold</button>
-            <button className="btn">333 Gold</button>
+            <button className="btn" onClick={firstbtn}>
+              900 Coin
+            </button>
+            <button className="btn" onClick={secondBtn}>
+              750 Gold
+            </button>
+            <button className="btn" onClick={thirdBtn}>
+              585 Gold
+            </button>
+            <button className="btn" onClick={fourthBtn}>
+              333 Gold
+            </button>
             <br />
-            <button type="submit" className="btn">
+            <button type="submit" onClick={handleSubmit} className="btn">
               Submit
             </button>
             <br />
           </div>
         </div>
         <div className="resultDisplayer">
-          <div className="heading">Price of 900 coins</div>
-          {/* <div className="headingdisplayeroutput"> */}
-          {/* </div> */}
+          <div className="heading goldRatesecondHeading ">
+            Jewelry Gold: €{inputedGoldPrice}
+            <br />
+            <h3 className="secondPriceHeading">Price of {active} gram</h3>
+          </div>
           <div className="resultOutput">
-            <div className="exactResult">512</div>
+            <div className="exactResult">
+              {(inputedGoldPrice / 1000) * active}€
+            </div>
           </div>
         </div>
       </div>
